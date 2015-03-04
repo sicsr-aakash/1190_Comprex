067952074e9c6c72213778df3db53f2dc2d75f23
12030121012 <12030121012@sicsr.ac.in>
2015-03-04 13:17:32 +0530

    App Fully Working
    ImageView to Bitmap Conversion
    Image Compression
    Saving to Internal Memory

--- app/src/main/java/com/prn12030121012/nikhilkedia/comprex/MainActivity.java
+++ app/src/main/java/com/prn12030121012/nikhilkedia/comprex/MainActivity.java
@@ -1,24 +1,40 @@
 package com.prn12030121012.nikhilkedia.comprex;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.BreakIterator;
+import java.util.BitSet;
+import java.util.Objects;
+import java.util.Random;
+
+import static java.util.Objects.requireNonNull;
 
 public class MainActivity extends Activity {
 
     private static final int SELECT_PICTURE = 1;
 
-    private String selectedImagePath;
     private ImageView img;
-    private TextView welcome;
+    private Bitmap resized;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -27,7 +43,7 @@ public class MainActivity extends Activity {
         Intent intent = getIntent();
         String name = getIntent().getStringExtra("fn");
         // Create the text view
-        welcome = (TextView) findViewById(R.id.welcome);
+        TextView welcome = (TextView) findViewById(R.id.welcome);
         welcome.setText("Welcome " + name);
 
         // Set the text view as the activity layout
@@ -35,13 +51,13 @@ public class MainActivity extends Activity {
 
         img = (ImageView)findViewById(R.id.iv);
 
-        ((Button) findViewById(R.id.si))
+        findViewById(R.id.si)
                 .setOnClickListener(new OnClickListener() {
                     public void onClick(View arg0) {
                         Intent intent = new Intent();
                         intent.setType("image/*");
                         intent.setAction(Intent.ACTION_GET_CONTENT);
-                        startActivityForResult(Intent.createChooser(intent,"Select Picture"),
+                        startActivityForResult(Intent.createChooser(intent, "Select Picture"),
                                 SELECT_PICTURE);
                     }
                 });
@@ -51,7 +67,7 @@ public class MainActivity extends Activity {
         if (resultCode == RESULT_OK) {
             if (requestCode == SELECT_PICTURE) {
                 Uri selectedImageUri = data.getData();
-                selectedImagePath = getPath(selectedImageUri);
+                String selectedImagePath = getPath(selectedImageUri);
                 System.out.println("Image Path : " + selectedImagePath);
                 img.setImageURI(selectedImageUri);
             }
@@ -66,5 +82,35 @@ public class MainActivity extends Activity {
         return cursor.getString(column_index);
     }
 
+    public void compress(View view) throws IOException {
+        img.buildDrawingCache();
+        Bitmap bmap = img.getDrawingCache();
+        Toast.makeText(getApplicationContext(),"Converted to Bitmap",Toast.LENGTH_SHORT).show();
+        Bitmap resized = Bitmap.createScaledBitmap(bmap, 200, 200, true);
+        Toast.makeText(getApplicationContext(),"Compressed",Toast.LENGTH_SHORT).show();
+        saveToInternalSorage(resized);
+    }
 
-}
+    public String saveToInternalSorage(Bitmap bitmapImage) {
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath = new File(directory, "profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+
+            fos = new FileOutputStream(mypath);
+
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(getApplicationContext(), "Saved to Internal Storage !", Toast.LENGTH_SHORT).show();
+        return directory.getAbsolutePath();
+
+    }
+    }
